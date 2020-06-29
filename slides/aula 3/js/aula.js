a13a3ffccf488db6e7ea2a21d1a893783d98f699
Julio Melo <julio.melo@imd.ufrn.br>
2020-06-29 11:28:01 -0300

    Removendo arquivos desnecessários do FA Refatorando algumas partes

--- slides/aula 3/js/aula.js
+++ slides/aula 3/js/aula.js
@@ -1,5 +1,4 @@
-/*daria para melhorar se a função fosse "loadiframe" e recebesse mais parametros como o id da div*/
-function loadIframe1(addr, div_id) {
+function loadIframe(addr, div_id) {
   var elem = $(div_id);
   var width_height = 'width="' + elem.width() + '" ' + 'height="'+elem.height() + '"';
   var src = 'src="' + addr+'"';
